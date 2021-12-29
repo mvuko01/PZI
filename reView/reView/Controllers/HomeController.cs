eff32249d1f081852df0ec3a7eca347461e7f3d5
mvuko01 <mvuko01@fesb.hr>
2021-12-29 13:24:42 +0100

    Cleaned up code

--- reView/reView/Controllers/HomeController.cs
+++ reView/reView/Controllers/HomeController.cs
@@ -11,22 +11,19 @@ namespace reView.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
