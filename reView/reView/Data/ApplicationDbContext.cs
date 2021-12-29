58af2494c5fccf4972c4a23f957152d5c15b1df6
mvuko01 <mvuko01@fesb.hr>
2021-12-29 13:40:23 +0100

    Connected to database

---
+++ reView/reView/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using reView.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reView.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ reView/reView/Migrations/20211229123902_spellingMistake.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace reView.Migrations
+{
+    public partial class spellingMistake : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Caregory",
+                table: "Caregory");
+
+            migrationBuilder.RenameTable(
+                name: "Caregory",
+                newName: "Category");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Category",
+                table: "Category",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Category",
+                table: "Category");
+
+            migrationBuilder.RenameTable(
+                name: "Category",
+                newName: "Caregory");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Caregory",
+                table: "Caregory",
+                column: "Id");
+        }
+    }
+}
